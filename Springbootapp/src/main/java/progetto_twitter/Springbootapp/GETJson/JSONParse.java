0defd8a0691946bcf97c2d40bfc2914adbc0c46f
RGiova <66753387+RGiova@users.noreply.github.com>
2020-07-14 19:49:29 +0200

    Revert "Revert "FIGa""
    This reverts commit 25de99173e1792a2144a87431f9c7e0700e2460c.

---
+++ Springbootapp/src/main/java/progetto_twitter/Springbootapp/GETJson/JSONParse.java
@@ -0,0 +1,26 @@
+package progetto_twitter.Springbootapp.GETJson;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+import progetto_twitter.Springbootapp.model.*;
+public class JSONParse {
+	private static JSONObject p1 = new JSONObject();
+public static void ParseText (JSONArray objp) {
+    Iterator i = objp.iterator();
+	for(int j = 0; j<=5; j++) {
+	  JSONModel Mdl = new JSONModel();
+      p1 = (JSONObject)i.next();
+      Mdl.setDate ((String)p1.get("created_at"));
+      Mdl.setText ((String)p1.get("full_text"));
+      Mdl.setHashtags(GetUrlsHashtags.GetHashtags((JSONObject)p1.get("entities")));
+      Mdl.setURL(GetUrlsHashtags.GetUrls((JSONObject)p1.get("entities")));
+      Mdl.setImg(GetImage.GetImg((JSONObject)p1.get("entities")));
+      ModelList.ListAdd(Mdl);
+
+	}
+}
+
+}
---
+++ Springbootapp/src/test/java/progetto_twitter/Springbootapp/SpringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package progetto_twitter.Springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
